704b16c68a3f1a10256c38cf89e54215af90b818
Sergi lona masriera <sergilmasriera@gmail.com>
2021-10-14 18:01:39 +0200

    login added , ma sharedPreferences da aggiustare

--- app/src/main/java/com/example/finalproject/SplashActivity.kt
+++ app/src/main/java/com/example/finalproject/SplashActivity.kt
@@ -1,22 +1,39 @@
 package com.example.finalproject
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var sharedPref : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         delay()
+        sharedPref = getSharedPreferences("login",MODE_PRIVATE)
+        if (sharedPref.getBoolean("logged",true)){
+            goToMain()
+        }else if(sharedPref == null || sharedPref.getBoolean("logged",false)){
+            goToLogin()
+        }
+
+
         finish()
     }
     fun delay(){
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
         }, 1000)
+    }
+
+    private fun goToMain() {
+        var intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToLogin(){
+        var intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
     }
 }
